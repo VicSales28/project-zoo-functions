e220d07bc4256b399879aa9d5c3fe732be79d088
Victoria Sales <contatovictoriasales@gmail.com>
2022-12-12 18:59:19 -0300

    C8-Implemente a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,29 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const numberOfAllResidentAnimals = species.reduce((acc, specie) => {
+  acc[specie.name] = specie.residents.length;
+  return acc;
+}, {});
+
+const residentsOfASpecificSpecie = (animal) => {
+  const { residents } = species.find(({ name }) => name === Object.values(animal)[0]);
+  return residents.length;
+};
+
+const residentsOfASpecificSex = (animal) => {
+  const { residents } = species.find(({ name }) => name === Object.values(animal)[0]);
+  const filter = residents.filter((resident) => resident.sex === Object.values(animal)[1]);
+  return filter.length;
+};
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === undefined) {
+    return numberOfAllResidentAnimals;
+  }
+  if (Object.values(animal).length === 1) {
+    return residentsOfASpecificSpecie(animal);
+  }
+  return residentsOfASpecificSex(animal);
 };
 
 module.exports = countAnimals;
