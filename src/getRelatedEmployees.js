0b3fd7ca38bc5f325b6871b43b5d2329dfc11476
Victoria Sales <contatovictoriasales@gmail.com>
2022-12-12 14:58:22 -0300

    C6-Ajusta função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,13 +1,13 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-const isManager = (id) => data.employees.some(({ managers }) => managers.includes(id));
+const isManager = (id) => employees.some(({ managers }) => managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
   const output = isManager(managerId);
   if (output === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  const managedEmployees = data.employees.filter(({ managers }) => managers.includes(managerId));
+  const managedEmployees = employees.filter(({ managers }) => managers.includes(managerId));
   const names = managedEmployees.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
   return names;
 };
