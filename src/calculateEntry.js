4c3478992b719e4d93022d3ac29f391664312dc9
Victoria Sales <contatovictoriasales@gmail.com>
2022-12-13 17:11:02 -0300

    Implementa a função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter((visitor) => visitor.age < 18).length;
+  const adult = entrants.filter((visitor) => visitor.age >= 18 && visitor.age < 50).length;
+  const senior = entrants.filter((visitor) => visitor.age >= 50).length;
+  return { child, adult, senior };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === 0 || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const childPrice = countEntrants(entrants).child * prices.child;
+  const adultPrice = countEntrants(entrants).adult * prices.adult;
+  const seniorPrice = countEntrants(entrants).senior * prices.senior;
+
+  const result = (childPrice + adultPrice + seniorPrice);
+
+  return result;
 };
+
+console.log(calculateEntry([
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]));
 
 module.exports = { calculateEntry, countEntrants };
