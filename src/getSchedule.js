b09a681928edc5321e5990436c96adff8b9b9458
Victoria Sales <contatovictoriasales@gmail.com>
2022-12-13 23:02:51 -0300

    Início da implementação da função getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,28 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
+
+const day = Object.keys(hours);
+
+const getObjWithAllSchedules = () => {
+  const objectWithAllSchedules = day.reduce((acc, day) => {
+    acc[day] = {
+      officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+      exhibition: species.filter((animal) => animal.availability.includes(day)).map(({ name }) => name),
+    };
+    return acc;
+  }, {});
+  objectWithAllSchedules.Monday = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+  return objectWithAllSchedules;
+};
+
+console.log(getObjWithAllSchedules());
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (scheduleTarget === undefined) {
+    return getObjWithAllSchedules();
+  }
 };
 
 module.exports = getSchedule;
