efdefea2e758e2e1cb45c9a091da41234d8defe5
Victoria Sales <contatovictoriasales@gmail.com>
2022-12-14 15:21:32 -0300

    C15-Conclui função getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,12 +1,14 @@
 const { species, hours } = require('../data/zoo_data');
 
-const day = Object.keys(hours);
+const days = Object.keys(hours);
 
 const getObjWithAllSchedules = () => {
-  const objectWithAllSchedules = day.reduce((acc, day) => {
+  const objectWithAllSchedules = days.reduce((acc, day) => {
     acc[day] = {
       officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
-      exhibition: species.filter((animal) => animal.availability.includes(day)).map(({ name }) => name),
+      exhibition: species
+        .filter((animal) => animal.availability.includes(day))
+        .map(({ name }) => name),
     };
     return acc;
   }, {});
@@ -23,6 +25,14 @@ const getSchedule = (scheduleTarget) => {
   if (scheduleTarget === undefined) {
     return getObjWithAllSchedules();
   }
+  const targetSpecie = species.find((specie) => specie.name === scheduleTarget);
+  if (!targetSpecie && !days.includes(scheduleTarget)) {
+    return getObjWithAllSchedules();
+  }
+  if (days.includes(scheduleTarget)) {
+    return { [scheduleTarget]: getObjWithAllSchedules()[scheduleTarget] };
+  }
+  return species.find((test) => test.name === scheduleTarget).availability;
 };
 
 module.exports = getSchedule;
