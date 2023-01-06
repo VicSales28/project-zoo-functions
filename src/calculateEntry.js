15776964cfc956d841c03c2a2b73209045d7ec26
Victoria Sales <contatovictoriasales@gmail.com>
2023-01-06 12:58:31 -0300

    C20-Refatora função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,8 +1,8 @@
 const { prices } = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
+const countEntrants = (arr) => {
   const ageCounter = { child: 0, adult: 0, senior: 0 };
-  return entrants.reduce((acc, curr) => {
+  return arr.reduce((acc, curr) => {
     if (curr.age < 18) {
       ageCounter.child += 1;
     } if (curr.age >= 18 && curr.age < 50) {
@@ -14,14 +14,14 @@ const countEntrants = (entrants) => {
   }, ageCounter);
 };
 
-const calculateEntry = (entrants) => {
-  if (entrants === undefined || entrants.length === 0 || Object.keys(entrants).length === 0) {
+const calculateEntry = (arr) => {
+  if (arr === undefined || arr.length === 0 || Object.keys(arr).length === 0) {
     return 0;
   }
-
-  const childPrice = countEntrants(entrants).child * prices.child;
-  const adultPrice = countEntrants(entrants).adult * prices.adult;
-  const seniorPrice = countEntrants(entrants).senior * prices.senior;
+  const { child, adult, senior } = countEntrants(arr);
+  const childPrice = child * prices.child;
+  const adultPrice = adult * prices.adult;
+  const seniorPrice = senior * prices.senior;
 
   const result = (childPrice + adultPrice + seniorPrice);
 
